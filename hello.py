717313b3606b9752cdc70606d71cbc54f2e68223
MrFlava <thatelitemaili33t@gmail.com>
2022-03-29 00:57:49 +0300

    Added hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# TODO: Create a Flask REST-API with:
+#  User model, auth
+#  Posts model, create/update/delete posts
+#  Comments thread
+#  README.md
+
 
 @app.route('/')
 def hello():
