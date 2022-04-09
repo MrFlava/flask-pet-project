89afbd9d28f87f8daf1ecb4d830b2be8ab38e06e
MrFlava <thatelitemaili33t@gmail.com>
2022-04-09 12:34:05 +0300

    Updated models.py, updated and renamed hello.py into main.py

--- models.py
+++ models.py
@@ -1,29 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import UserMixin
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 db = SQLAlchemy()
 
-roles_users_table = db.Table(
-    "roles_users",
-    db.Column("users_id", db.Integer(), db.ForeignKey("users.id")),
-    db.Column("roles_id", db.Integer(), db.ForeignKey("roles.id")),
-)
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(64))
 
-class Roles(db.Model):
-    __tablename__ = "roles"
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
 
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
 
-class Users(db.Model, UserMixin):
-    __tablename__ = "users"
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
 
-    id = db.Column(db.Integer(), primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(80))
-    active = db.Column(db.Boolean())
-
-    roles = db.relationship("Roles", secondary=roles_users_table, backref="user", lazy=True)
